736ece0a9de78324c9355fbd660ad25f81ff123e
gemAbhishek <ak.abhishekshah@gmail.com>
2020-09-10 10:59:24 +0530

    valueController added

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApi_CI_test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly string[] val = new[]
+        {
+            "value1","value22","value33"
+        };
+
+
+        [HttpGet]
+        public string[] getall()
+        {
+            return val;
+        }
+    }
+}
